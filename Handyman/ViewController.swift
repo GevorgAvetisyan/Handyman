7ad5b4ee15a5bad26fca45597512a403357fac39
Gev <gevoravetisyan11@gmail.com>
2019-10-11 17:02:46 +0400

    Initial commit

---
+++ Handyman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Handyman
+//
+//  Created by Gev Avetisyan on 10/11/19.
+//  Copyright © 2019 Gev Avetisyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
